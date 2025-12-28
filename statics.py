b116ed31e8847e0f097c8f454758503d9ad6942c
lbrasil73 <almeidaleandro.silva@gmail.com>
2025-12-28 11:05:07 -0300

    featrure: add examples with pivot

--- statics.py
+++ statics.py
@@ -8,9 +8,22 @@ region = data.groupby("Region")['Revenue'].sum()
 
 product = data.groupby(["Region" ])["Units Sold"].sum()
 # product2= data.groupby(["Product"])["Units Sold"].sum().plot(kind='bar')
+productMult = data.groupby('Product').agg( {'Revenue': ['sum', 'mean']})
 
 # data.plot(x="Region", y="Revenue", kind="bar")
 # plt.show()
-product.plot( kind="bar", title="title")
-plt.ylabel("Revenue")
-plt.show()
+
+# product.plot( kind="bar", title="title")
+# plt.ylabel("Revenue")
+# plt.show()
+
+# productMult.plot( kind='bar',  title='title')
+# # plt.ylabel('Region')
+# plt.show()
+
+# revenue more 100
+# revenueMore100 = data.groupby('Product').apply( lambda g: g[g['Revenue'] > 100 ]
+#                                                , include_groups=False)
+# print( revenueMore100)
+
+soldMore10 = data.groupby('Revenue').apply( lambda g: g +  100)
