1ba8fbf7e5ce6169766941d80bfddb936acde0eb
lbrasil73 <almeidaleandro.silva@gmail.com>
2026-01-06 18:47:07 -0300

    feature: change date for datetime

--- clean-02.py
+++ clean-02.py
@@ -11,6 +11,7 @@ print('========== original============')
 print('========== cleaning=============')
 
 # insert data with null - previous date
+data['Date'] = pd.to_datetime( data[ 'Date' ], errors='coerce' )
 data['Date'] = data['Date'].ffill()
 
 # change value null for ohters
@@ -21,6 +22,7 @@ missingValueDate = data[ data[ 'Customer_Name' ].isnull() ]
 
 # change $ of Price for space 
 data['Price'] = data['Price'].str.strip().str.replace( "$", " ")
+# print("info - \n", data.info() )
 
-print( data['Price'])
+print( data)
 # print("Missing value: \n", data.isnull().sum() )
