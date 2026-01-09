64340a10c727a605a43801fd3de90fc991d81542
lbrasil73 <almeidaleandro.silva@gmail.com>
2026-01-08 21:52:49 -0300

    feature: change value nan in price for quantity correspond value

--- clean-02.py
+++ clean-02.py
@@ -16,13 +16,26 @@ data['Date'] = data['Date'].ffill()
 
 # change value null for ohters
 data['Customer_Name'] = data['Customer_Name'].fillna( 'others')
-
 missingValueDate = data[ data[ 'Customer_Name' ].isnull() ]
-
+data['Customer_Name'] = data['Customer_Name'].str.lower().str.strip()
 
 # change $ of Price for space 
 data['Price'] = data['Price'].str.strip().str.replace( "$", " ")
-# print("info - \n", data.info() )
+data['Price'] = data['Price'].str.strip().str.replace( "not_set", "000")
+data['Price'] = data['Price'].astype(float)
 
-print( data)
-# print("Missing value: \n", data.isnull().sum() )
+groupyPriceQuant = data.groupby('Quantity')['Price'].transform( 'mean')
+data['Price'] = data['Price'].fillna( groupyPriceQuant )
+missingValuePrice = data[ data[ 'Price'].isnull() ]
+
+
+data['Product_Category'] = data['Product_Category'].str.strip().str.lower()
+data['Product_Category'] = data['Product_Category'].str.replace('???', 'olther_category')
+
+
+
+# print("info - \n", data.info() )
+# print( data.head() )
+# print( data['Price'] == 0  )
+print( missingValuePrice )
+# print("Missing value: \n", data.isnull().sum()
